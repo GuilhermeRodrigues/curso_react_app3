3e7393af92cdee1afa425f7ecac3397a85968f0d
GuilhermeRodrigues <guilherme17_rodrigues@hotmail.com>
2019-03-26 22:06:55 -0300

    Aula props
    Praticando props.

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
-/**
- * @format
- */
+import React, { Component } from 'react';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
 
-AppRegistry.registerComponent(appName, () => App);
+
+class MeuComponente extends Component {
+    render(){
+        return (
+            <View>
+                <Text>{this.props.propriedade1}</Text>
+                <Text>{this.props.xyz}</Text>
+                <Text>{this.props.p}</Text>
+            </View>
+        );
+    }
+
+}
+
+class app3 extends Component {
+    
+    render(){
+        return (
+            <MeuComponente propriedade1='Banana' xyz='Abacaxi' p='Uva'></MeuComponente>
+        );
+    }
+}
+
+AppRegistry.registerComponent('app3', () => app3);

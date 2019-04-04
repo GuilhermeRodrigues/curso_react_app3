910ce80f287527f066a18ebae869d7559e7fe1dc
GuilhermeRodrigues <guilherme17_rodrigues@hotmail.com>
2019-04-03 22:32:46 -0300

    Lógica do jogo - partes 2 e 3
    Codificando a lógica do jogo - partes 2 e 3.

--- index.js
+++ index.js
@@ -13,19 +13,79 @@ class app3 extends Component {
     constructor(props){
         super(props);
 
-        this.state = { escolhaUsuario: '' }
+        this.state = { escolhaUsuario: '', escolhaComputador : '', resultado : '' }
     }
 
     jokenpo(escUsuario){
-        this.setState({ escolhaUsuario : escUsuario });
+
+        //gera número aleatório (0, 1, 2)
+        var numAleatorio = Math.floor(Math.random() * 3);
+
+        var escComputador = '';
+
+        switch(numAleatorio){
+            case 0: escComputador = 'pedra'; break;
+            case 1: escComputador = 'papel'; break;
+            case 2: escComputador = 'tesoura'; break;
+        }
+
+        var result = '';
+
+        if (escComputador == 'pedra'){
+            if (escUsuario == 'pedra'){
+                result = 'Empate';
+            }
+
+            if (escUsuario == 'papel'){
+                result = 'Usuário ganhou';
+            }
+
+            if (escUsuario == 'tesoura'){
+                result = 'Computador ganhou';
+            }
+        }
+
+        if (escComputador == 'papel'){
+            if (escUsuario == 'papel'){
+                result = 'Empate';
+            }
+
+            if (escUsuario == 'tesoura'){
+                result = 'Usuário ganhou';
+            }
+
+            if (escUsuario == 'pedra'){
+                result = 'Computador ganhou';
+            }
+        }
+
+        if (escComputador == 'tesoura'){
+            if (escUsuario == 'tesoura'){
+                result = 'Empate';
+            }
+
+            if (escUsuario == 'pedra'){
+                result = 'Usuário ganhou';
+            }
+
+            if (escUsuario == 'papel'){
+                result = 'Computador ganhou';
+            }
+        }
+
+        this.setState({ escolhaUsuario : escUsuario,
+                        escolhaComputador : escComputador,
+                        resultado : result  
+                    });
+        
     }
     
     render(){
         return (
             <View>
-                <Text>Escolha do computador</Text>
+                <Text>Escolha do computador: {this.state.escolhaComputador}</Text>
                 <Text>Escolha do usuário: {this.state.escolhaUsuario}</Text>
-                <Text>Resultado</Text>
+                <Text>Resultado: {this.state.resultado}</Text>
                 <Button title="pedra" onPress={ () => { this.jokenpo('pedra')}} />
                 <Button title="papel" onPress={ () => { this.jokenpo('papel')}} />
                 <Button title="tesoura" onPress={ () => { this.jokenpo('tesoura')}} />
